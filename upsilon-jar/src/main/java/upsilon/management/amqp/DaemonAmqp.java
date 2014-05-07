0d90d3e17583762125b74fd1a3a8659b3589b532
jamesread <jread@redhat.com>
2014-05-08 00:24:53 +0100

    It works!

--- upsilon-jar/src/main/java/upsilon/management/amqp/DaemonAmqp.java
+++ upsilon-jar/src/main/java/upsilon/management/amqp/DaemonAmqp.java
@@ -39,17 +39,16 @@ public class DaemonAmqp extends Daemon implements Runnable {
 	}
 
 	private static final transient Logger LOG = LoggerFactory.getLogger(DaemonAmqp.class);
-	private QueueingConsumer consumer;
+	private QueueingConsumer consumerRecv;
 
 	private boolean run = true;
 
-	private Channel channel;
+	private Channel channelRecv;
 
 	private final String EXCHANGE_NAME = "ex_upsilon";
 
-	private final String ROUTING_KEY = "upsilon";
-
-	private String QUEUE_NAME;
+	private String QUEUE_NAME_RECV;
+	private Connection connection;
 
 	public DaemonAmqp() throws Exception {
 
@@ -58,6 +57,18 @@ public class DaemonAmqp extends Daemon implements Runnable {
 
 	private String generateQueueName() {
 		return "upsilon-node-" + UUID.randomUUID().toString();
+	}
+
+	private BasicProperties getNewMsgProps(UpsilonMessageType messageType) {
+		Map<String, Object> headers = new HashMap<>();
+		headers.put("upsilon-msg-type", messageType.toString());
+		headers.put("version", Main.getVersion());
+		headers.put("identifier", Main.instance.node.getIdentifier());
+
+		Builder builder = new BasicProperties.Builder();
+		builder.headers(headers);
+
+		return builder.build();
 	}
 
 	private void handleMessageType(UpsilonMessageType type, String body, long deliveryTag, String replyTo) throws IOException {
@@ -70,42 +81,34 @@ public class DaemonAmqp extends Daemon implements Runnable {
 
 			DaemonAmqp.LOG.debug("req version summary, sending back:\n" + nodeSummary);
 
-			this.publish(UpsilonMessageType.RES_NODE_SUMMARY, nodeSummary);
-			this.channel.basicAck(deliveryTag, false);
+			BasicProperties props = this.getNewMsgProps(UpsilonMessageType.RES_NODE_SUMMARY);
+
+			Channel channelSend = this.connection.createChannel();
+			channelSend.queueBind(replyTo, this.EXCHANGE_NAME, "upsilon.res");
+			channelSend.basicPublish(this.EXCHANGE_NAME, "upsilon.res", props, nodeSummary.getBytes());
+			channelSend.close();
+
+			this.channelRecv.basicAck(deliveryTag, false);
 			break;
 		case RES_NODE_SUMMARY:
 			DaemonAmqp.LOG.debug("res version summary:\n" + body);
-			this.channel.basicAck(deliveryTag, false);
+			this.channelRecv.basicAck(deliveryTag, false);
 			break;
 		case UNKNOWN:
-			this.channel.basicAck(deliveryTag, false);
+			this.channelRecv.basicAck(deliveryTag, false);
 			break;
 		default:
 			DaemonAmqp.LOG.warn("Unsupported upsilon message type: " + type);
 		}
 	}
 
-	private void publish(UpsilonMessageType messageType, byte[] body) throws IOException {
-		Map<String, Object> headers = new HashMap<>();
-		headers.put("upsilon-msg-type", messageType.toString());
-
-		Builder builder = new BasicProperties.Builder();
-		builder.headers(headers);
-
-		this.channel.basicPublish(this.EXCHANGE_NAME, this.ROUTING_KEY, builder.build(), body);
-	}
-
-	private void publish(UpsilonMessageType messageType, String body) throws IOException {
-		this.publish(messageType, body.getBytes());
-	}
-
 	@Override
 	public void run() {
 		try {
-			this.channel.basicConsume(this.QUEUE_NAME, false, "upsilon-node Message Consumer", this.consumer);
+			this.channelRecv.basicConsume(this.QUEUE_NAME_RECV, false, "upsilon-node Message Consumer", this.consumerRecv);
 
 			while (this.run) {
-				QueueingConsumer.Delivery delivery = this.consumer.nextDelivery();
+				QueueingConsumer.Delivery delivery = this.consumerRecv.nextDelivery();
 
 				String body = new String(delivery.getBody());
 				Map<String, Object> headers = delivery.getProperties().getHeaders();
@@ -138,21 +141,21 @@ public class DaemonAmqp extends Daemon implements Runnable {
 		factory.setHost(hostname);
 
 		try {
-			Connection connection = factory.newConnection();
-			this.channel = connection.createChannel();
-
-			this.channel.exchangeDeclare(this.EXCHANGE_NAME, "fanout");
-
-			this.QUEUE_NAME = this.generateQueueName();
-
 			Map<String, Object> queueArgs = new HashMap<String, Object>();
 			queueArgs.put("upsilon-version", Main.getVersion());
 			queueArgs.put("upsilon-node-identifier", Main.instance.node.getIdentifier());
 
-			this.channel.queueDeclare(this.QUEUE_NAME, false, false, true, queueArgs);
-			this.channel.queueBind(this.QUEUE_NAME, this.EXCHANGE_NAME, this.ROUTING_KEY);
+			this.connection = factory.newConnection();
+			Channel channelAdmin = this.connection.createChannel();
+			channelAdmin.exchangeDeclare(this.EXCHANGE_NAME, "topic");
+			channelAdmin.close();
 
-			this.consumer = new QueueingConsumer(this.channel);
+			this.QUEUE_NAME_RECV = this.generateQueueName();
+
+			this.channelRecv = this.connection.createChannel();
+			this.channelRecv.queueDeclare(this.QUEUE_NAME_RECV, false, false, true, queueArgs);
+			this.channelRecv.queueBind(this.QUEUE_NAME_RECV, this.EXCHANGE_NAME, "upsilon.cmds");
+			this.consumerRecv = new QueueingConsumer(this.channelRecv);
 		} catch (Exception e) {
 			DaemonAmqp.LOG.error("Could not complete initial AMQP binding: " + e.getMessage(), e);
 			return;
