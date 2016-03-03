a1feda3851ba6d2c1f9b9c9cd80b76b888dd14ed
jamesread <contact@jwread.com>
2016-03-03 16:08:05 +0000

    .

--- var/smoketests/unpackaged-el7/startup.sh
+++ var/smoketests/unpackaged-el7/startup.sh
@@ -10,6 +10,6 @@ trap onErr ERR
 
 UUID=$(docker create upsilon-node)
 docker start $UUID
-docker exec -it $UUID /usr/share/upsilon-node/bin/tools/upsilon-test-envionment
+docker exec -i $UUID /usr/share/upsilon-node/bin/tools/upsilon-test-envionment
 docker stop $UUID
 docker rm $UUID
