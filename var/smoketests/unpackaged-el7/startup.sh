c251a22a6efb980b9515d3d8cc38a98dc2b6149f
jamesread <contact@jwread.com>
2016-03-03 14:35:04 +0000

    .

--- var/smoketests/unpackaged-el7/startup.sh
+++ var/smoketests/unpackaged-el7/startup.sh
@@ -2,6 +2,8 @@
 UUID=`uuidgen`
 docker run -itd --name $UUID upsilon-node
 docker cp tests.sh $UUID:/opt/
+docker create $UUID
+docker start $UUID
 docker exec $UUID /opt/tests.sh
 docker stop $UUID
 docker rm $UUID
--- var/smoketests/unpackaged-el7/tests.sh
+++ var/smoketests/unpackaged-el7/tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/share/upsilon-node/bin/upsilon-node
+/usr/bin/ps aux
