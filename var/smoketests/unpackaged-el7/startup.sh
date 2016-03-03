fe452307874cc57b5726ec4895d0bd42b0db4cc2
jamesread <contact@jwread.com>
2016-03-03 17:08:44 +0000

    Added a delay to give upsilon-node a chance to startup.

--- var/smoketests/unpackaged-el7/startup.sh
+++ var/smoketests/unpackaged-el7/startup.sh
@@ -10,6 +10,9 @@ trap onErr ERR
 
 UUID=$(docker create upsilon-node)
 docker start $UUID
+
+sleep 5 # upsilon-node needs time to start it's internals for testing
+
 docker exec -i $UUID /usr/share/upsilon-node/bin/tools/upsilon-test-envionment
 docker stop $UUID
 docker rm $UUID
